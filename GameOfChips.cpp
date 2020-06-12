196338a46f307fe3c67a7226d4d0b522bfd304cc
Kayvan Mivehnejad <kayvan_kukoc@yahoo.com>
2020-06-12 18:41:28 +0000

    Players turn to pick chips less than max allowed

--- GameOfChips.cpp
+++ GameOfChips.cpp
@@ -7,25 +7,56 @@ using namespace std;
 
 int main() {
 
+bool gameOver =false ;
+bool player1turn = true;  
+
 srand(time(0));
 const int numc = 100;
 int numOfInitialChips = 0;
 int numChipsTaken = 0;
 int MaxChipSelection = 0;
 int numChipsLeft = 0 ;
+
+string playerName[2] ;
+cout << "Welcome player one , what is your name? " << endl;
+cin >> playerName[0];
+cout << "Welcome player two , what is your name? " << endl;
+cin >> playerName[1];
+
+
+
 numOfInitialChips = static_cast<int>(rand()%numc)+1 ;
 cout << "Game of chips , starting with "<< numOfInitialChips << endl;
 
 MaxChipSelection = floor(numOfInitialChips* 0.5);
 
 
+do{
+do{
+  if (player1turn){
+  cout << "Hello, " << playerName[0] << endl;
+}else{
+  cout << "Hello, " << playerName[1] << endl;
+}
 cout << "How many chips you want?  up to: "<< MaxChipSelection << endl ;
 cin >> numChipsTaken ;
+
+ cout << endl ;
+  if(numChipsTaken > MaxChipSelection){
+    cout<< "Select a number less than "<< MaxChipSelection << endl;
+  }
+}while (numChipsTaken > MaxChipSelection);  
+
+
 numChipsLeft = numOfInitialChips - numChipsTaken;
-
+MaxChipSelection = floor(numChipsLeft* 0.5);
 cout << "now we have " << numChipsLeft << " chips in the pot" << endl; 
+  numOfInitialChips = numChipsLeft ;
+  player1turn =! player1turn;
 
-//numChipsTaken = static_cast<int>((rand()%MaxChipSelection)+1);
-
+} 
+while(
+  numChipsLeft > 0
+  );
 
 }
