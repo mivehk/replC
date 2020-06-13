7332b758e28de90fde6bbfe5acaa374911527d7f
Kayvan Mivehnejad <kayvan_kukoc@yahoo.com>
2020-06-13 19:55:42 +0000

    cool stuff!

---
+++ CS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+    cout << "************************************" << endl ;
+    cout << "                                    " << endl ;
+    cout << "      cccc         ssss      !!     " << endl ;
+    cout << "     c           s           !!     " << endl ;
+    cout << "    c            s           !!     " << endl;
+    cout << "   c              ssss       !!     " << endl;
+    cout << "    c                 s      !!     " << endl;
+    cout << "     c                s             " << endl;
+    cout << "      cccc        ssss       00     " << endl;
+    cout << "                                    " << endl;
+    cout << "************************************" << endl;
+    cout << " Computer Science is Cool Stuff!!!"   << endl;
+
+
+    return 0;
+
+}
