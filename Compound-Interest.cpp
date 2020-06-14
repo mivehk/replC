1814a5f286592b9f66fbb7ceee04f4e398e51592
Kayvan Mivehnejad <kayvan_kukoc@yahoo.com>
2020-06-14 17:46:37 +0000

    add precision to FB

--- Compound-Interest.cpp
+++ Compound-Interest.cpp
@@ -4,20 +4,25 @@
 #include <math.h>
 
 using namespace std;
+//using namespace ios;
 
 int main() {
-  float finalBalance = 0;
+  double finalBalance = 0;
   float principal = 0;
   int shInterestRate = 6;
   float interestRate = (float)shInterestRate / 100;
-  cout << interestRate << endl ; 
+  //cout << interestRate << endl ; 
   int times = 1 ;
   int compound = 2 ;
 
-  cout << "Hello customer, our interest rate is %" << shInterestRate <<" which is compounded " << compound <<" times over the course of year" << endl ;
-  cout << "Now ,How much money you are interested in investing? " << endl;
+  cout.setf(ios::fixed);
+  cout.setf(ios::showpoint);
+  cout.precision(2);
+
+  cout << "Hello customer, our interest rate is %" << shInterestRate <<" which is compounded " << compound <<" times over the course of year\n" ;
+  cout << "Now ,How much money you are interested in investing? \n";
   cin >> principal ;
   finalBalance = principal*(pow((1 + interestRate/compound),(compound*times )));
-  cout << "Your final balance in one year would be "<< finalBalance << endl;
+  cout << "Your final balance in one year would be $"<< finalBalance << endl;
 
 }
