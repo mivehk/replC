609652fbe3ade72934745ccc4e15b9cd56070671
mivehk <k.mivehnejad@gmail.com>
2020-06-28 13:26:31 -0400

    d'aujourd'hui

--- fahrenheit-to-celsius.cpp
+++ fahrenheit-to-celsius.cpp
@@ -7,30 +7,27 @@ void get_input(double& fah_par);
 
 int main(){
 
-using namespace std;
-double fahrenheit , celsius;
+    using namespace std;
+    double fahrenheit , celsius;
 
-initialize();
+    initialize();
 
-cout<< "bonjour \n";
-cout << "vous allez bien? \n";
+    cout<< "Bonjour \n";
+    cout << "Vous allez bien? \n";
 
+    get_input(fahrenheit);
+    celsius = convert_f2c(fahrenheit);
+    show_result ( fahrenheit , celsius);
+    cout << endl ;
 
-get_input(fahrenheit);
-
-celsius = convert_f2c(fahrenheit);
-
-show_result ( fahrenheit , celsius);
-
-cout << endl ;
 return 0;
 }
 
 void get_input(double& fah_par){
     using namespace std;
-    cout << "quelle est la temperature \n";
+    cout << "Quelle est la temperature d'aujourd'hui? \n";
     cin >> fah_par;
-    cout << "je ne comprends pas; je demande a' l'ordinateur \n \n";
+    cout << "Je ne comprends pas; je demande a` l'ordinateur \n \n";
 }
 
 
@@ -54,6 +51,6 @@ void show_result( double fah_par, double cel_par){
     cout.setf(ios::showpoint);
     cout.precision(1);
     cout << "Temperature is "<< fah_par 
-    << " fahrenheit which is close to " << cel_par
-    << " celsius";
+    << " Fahrenheit which is close to " << cel_par
+    << " Celsius";
 }
