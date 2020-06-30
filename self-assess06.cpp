edf6308fbbe28a949acd05db1bb7cd7325aace8e
mivehk <k.mivehnejad@gmail.com>
2020-06-30 16:43:16 -0400

    output file name asked

--- self-assess06.cpp
+++ self-assess06.cpp
@@ -9,6 +9,14 @@ using namespace std;
 ifstream in_str;
 ofstream out_str;
 int first, second, third, forth, fifth;
+char out_file[16];
+
+cout<< " this program takes numbers recorded by input file \n"
+<< " then calculate their sum in the file you pick\n"
+<<"now tell me the name of output file with max 15 characters: ";
+
+cin>>out_file ;
+
 
 in_str.open("input06.txt");
 if (in_str.fail()){
@@ -17,8 +25,8 @@ if (in_str.fail()){
 }
 in_str >> first >> second >> third >> forth >> fifth;
 
-out_str.open("output06.txt");
-if (in_str.fail()){
+out_str.open(out_file , ios::app);
+if (out_str.fail()){
     cout << " error has happened while reading outfile stream";
     exit(1);
 }
@@ -28,6 +36,8 @@ int sum;
 sum = first + second + third + forth + fifth ;
 
 out_str << sum ;
+cout << sum << endl;
+
 
 in_str.close();
 out_str.close();
