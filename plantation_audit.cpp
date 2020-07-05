65e6512169b50c4716812f9245dbcb0e3ba13d31
mivehk <k.mivehnejad@gmail.com>
2020-07-05 17:37:37 -0400

    First commit

---
+++ plantation_audit.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void get_total(int num_par);
+void input_data(int a[], int num_par);
+
+int main(){
+
+int num_plantations = 0;
+
+cout << " Tell me how many plantations subscribed for audit? ";
+cin >> num_plantations;
+
+int plantations[num_plantations];
+input_data(plantations , num_plantations);
+
+    return 0;
+}
+
+void input_data(int a[], int num_plan_par){
+    for (int i=0 ; i < num_plan_par ; i++){
+        get_total(num_plan_par);
+    }
+    //cout << " total for plantation is " << get_total(num_plan_par) << endl ;
+}
+
+void get_total(int num_par){
+    int sum =0;
+    cout << " Enter number for each departments production count \n"
+    << " Ending list with a negative number \n";
+    while (num_par > 0){
+      cin >> num_par ;
+      sum = sum + num_par ;  
+    }
+    cout << " Total is: " << sum << endl ;
+
+}
