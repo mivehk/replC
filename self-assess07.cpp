5fa5239a50b7d22b7986739e64a65ebac508a642
mivehk <k.mivehnejad@gmail.com>
2020-07-21 21:55:42 -0400

    partial array fill with new search

--- self-assess07.cpp
+++ self-assess07.cpp
@@ -196,113 +196,203 @@ for(int i=0 ; i<num ; i++){
 }
 */
 
+// #include <iostream>
+// #include <cctype>
+// using namespace std;
+// void fill_array(int a[], int size_par , int& num_filled_par);
+// double calc_avg( int a[], int num_filled_par);
+// void show_diff(int a[], int num_filled_par);
+// int search(int a[], int num_filled_par , int quoi);
+// void skip_it();
+
+// int main(){
+
+
+// int  max_size ,num_size;
+
+// cout << " Tell me how many vineyards you visited: ";
+// cin >> max_size;
+
+// int vineyards[max_size];
+
+// /*int num_size;
+// cout << " Tell me how many pages you want to use of " << max_size << endl;
+// cin>> num_size;
+// */
+
+// fill_array(vineyards, max_size ,num_size);
+// show_diff( vineyards,num_size);
+
+//    char conf;
+// do {
+//     int quand;
+//     cout << " Now tell me which purchase you want to double check? ";
+//     cin >> quand;
+//     int t ;
+//     t = search( vineyards , num_size , quand);
+//     if (t == -1){
+//         cout << " We could not find this purchase in vineyards list , try again";
+//     } else {
+//         cout << " Your search for "<< quand <<" bottles was from vineyards# " << t <<endl;
+//     }
+//     cout << " Do you want to search for another purchase? (Yes/no): ";
+//      cin >> conf;
+//      skip_it();
+// } 
+// while (
+//     (conf !='n')&&(conf !='N')
+// );
+
+//     return 0;
+// }
+
+// void fill_array(int a[], int size_par , int& num_filled_par){
+//    /*  for (int i=0 ; i< size_par ; i++){
+//         cin >> a[i];
+//         } */
+
+//         int next , i=0;
+//         cout << " Give me total bottles purchased from each Vineyard \n" 
+//         << " ending your list with negative number when asked about vineyard number " << (size_par+1) << endl;
+//         cout << "Now tell me how many bottles from vineyards " << i+1 << " : " ;
+//         cin >> next;
+//         while ((next >=0)&&(num_filled_par < size_par)){
+//             a[i]=next;
+//             i++;
+//             cout << "Now for Vineyard " << i+1 << " : " ;
+//             cin >> next;
+//         }
+//         num_filled_par = i;
+// }
+
+// double calc_avg( int a[], int num_filled_par){
+//     double total =0.0; 
+//     for (int i=0 ; i< num_filled_par; i++){
+//       total = total + a[i]; 
+//     }
+//     if (num_filled_par >0){
+//        return ( total/num_filled_par) ;
+//     } 
+//     else{ 
+//         cout << " you need to enter a positive integer for size\n";
+//         }
+//        return 0; 
+// }
+
+// void show_diff(int a[], int num_filled_par){
+//      double average_is = 0.0 ;
+//      average_is= calc_avg( a , num_filled_par);
+//     for ( int i=0 ; i<num_filled_par ; i++){
+//         cout << " difference from vineyards number " << i+1 <<" purchased bottles of "<< a[i]<< " to average "<< average_is<< " is "
+//         << (a[i] - average_is) << endl ;
+//     }
+// }
+
+// int search(int a[], int num_filled_par , int quoi){
+//     bool found =false;
+//     int i=0 ;
+//     while((!found)&&(i<num_filled_par)){
+//         if (quoi == a[i]){
+//             return i+1;
+//         }
+//         i++;
+//     } return -1;
+// }
+
+// void skip_it(){
+//     char symbol;
+//     do { cin.get(symbol);} 
+//     while(
+//         (! isspace(symbol))
+//     );
+// }
+
 #include <iostream>
 #include <cctype>
 using namespace std;
-void fill_array(int a[], int size_par , int& num_filled_par);
-double calc_avg( int a[], int num_filled_par);
-void show_diff(int a[], int num_filled_par);
-int search(int a[], int num_filled_par , int quoi);
+
+
+void create_array(int a[] , int size_par , int& used_size_par);
+double calc_avg(int a[] , int used_size_par);
+void show_diff(int a[] , int used_size_par);
+int search(int a[], int search_par , int used_size_par);
 void skip_it();
 
 int main(){
+   int max_classes;
+   cout << "How many classes you took this semester? ";
+   cin >> max_classes;
 
+   int used_size;
+   int classes[max_classes];
 
-int  max_size ,num_size;
+   create_array( classes, max_classes,used_size );
+   show_diff(classes , used_size);
+   char sym;
+   int grade;
+   do{
+       cout << " tell me the grade which you are searching for :";
+       cin >> grade;
+       int t = search( classes , grade , used_size);
+       if ( t == -1){
+           cout << " couldn't find your grade" <<endl;
+       }else{
+           cout << " we found your grade " << grade << " at position " << t+1<<endl;
+       }
+       cout << " do you want to run another search? (Yes/No) ";
+       cin >> sym;
+       skip_it();
+   }while(
+           (sym !='n')&&( sym !='N')
+           );
 
-cout << " Tell me how many vineyards you visited: ";
-cin >> max_size;
-
-int vineyards[max_size];
-
-/*int num_size;
-cout << " Tell me how many pages you want to use of " << max_size << endl;
-cin>> num_size;
-*/
-
-fill_array(vineyards, max_size ,num_size);
-show_diff( vineyards,num_size);
-
-   char conf;
-do {
-    int quand;
-    cout << " Now tell me which purchase you want to double check? ";
-    cin >> quand;
-    int t ;
-    t = search( vineyards , num_size , quand);
-    if (t == -1){
-        cout << " We could not find this purchase in vineyards list , try again";
-    } else {
-        cout << " Your search for "<< quand <<" bottles was from vineyards# " << t <<endl;
-    }
-    cout << " Do you want to search for another purchase? (Yes/no): ";
-     cin >> conf;
-     skip_it();
-} 
-while (
-    (conf !='n')&&(conf !='N')
-);
 
     return 0;
 }
 
-void fill_array(int a[], int size_par , int& num_filled_par){
-   /*  for (int i=0 ; i< size_par ; i++){
-        cin >> a[i];
-        } */
-
-        int next , i=0;
-        cout << " Give me total bottles purchased from each Vineyard \n" 
-        << " ending your list with negative number when asked about vineyard number " << (size_par+1) << endl;
-        cout << "Now tell me how many bottles from vineyards " << i+1 << " : " ;
+void create_array(int a[] , int size_par , int& used_size_par){
+    int next , index =0;
+    cout<< " Grade entry for each course until "<< size_par+1 << " with negative number \n";
+    cout << " Now enter your grade for course #" << index+1 <<" : ";
+    cin >> next ;
+    do {
+        a[index]=next;
+        index++;
+        cout << " Now time to enter your course #" << index+1 << " : ";
         cin >> next;
-        while ((next >=0)&&(num_filled_par < size_par)){
-            a[i]=next;
-            i++;
-            cout << "Now for Vineyard " << i+1 << " : " ;
-            cin >> next;
-        }
-        num_filled_par = i;
+    }while(
+            ((used_size_par<size_par)&&(next>0))
+            );
+    used_size_par = index ;
 }
-
-double calc_avg( int a[], int num_filled_par){
-    double total =0.0; 
-    for (int i=0 ; i< num_filled_par; i++){
-      total = total + a[i]; 
+double calc_avg(int a[] , int used_size_par){
+    double avg =0.0;
+    int total=0 ;
+    for (int i=0 ; i<used_size_par ; i++){
+        total = total + a[i];
     }
-    if (num_filled_par >0){
-       return ( total/num_filled_par) ;
-    } 
-    else{ 
-        cout << " you need to enter a positive integer for size\n";
-        }
-       return 0; 
-}
+    return total/(used_size_par+1);
 
-void show_diff(int a[], int num_filled_par){
-     double average_is = 0.0 ;
-     average_is= calc_avg( a , num_filled_par);
-    for ( int i=0 ; i<num_filled_par ; i++){
-        cout << " difference from vineyards number " << i+1 <<" purchased bottles of "<< a[i]<< " to average "<< average_is<< " is "
-        << (a[i] - average_is) << endl ;
+}
+void show_diff(int a[] , int used_size_par){
+    double avg = calc_avg(a , used_size_par);
+    for (int i=0 ; i<used_size_par ; i++){
+        cout << a[i] << " is your grade #" << i+1<< " and it differ "<<a[i]-avg<< " from average grade of "<<avg << endl;
     }
-}
 
-int search(int a[], int num_filled_par , int quoi){
-    bool found =false;
-    int i=0 ;
-    while((!found)&&(i<num_filled_par)){
-        if (quoi == a[i]){
-            return i+1;
-        }
-        i++;
-    } return -1;
+}
+int search(int a[], int search_par , int used_size_par){
+    //bool found =false;
+    for (int i=0 ; i < used_size_par ; i++){
+            if (a[i] == search_par){
+            return i;
+            }
+    }
+    return -1;
 }
 
 void skip_it(){
     char symbol;
-    do { cin.get(symbol);} 
-    while(
-        (! isspace(symbol))
-    );
+    do {cin.get(symbol);}
+    while(! isspace(symbol));
 }
